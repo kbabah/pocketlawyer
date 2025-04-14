89297e2edf8ae2e70051aa9bbe003b5590d829f0
Kingsley K Babah <kbabah11@gmail.com>
2025-04-15 00:20:20 +0200

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ try {
   }
 }
 
-import * as pdfjsDist from 'pdfjs-dist';
+import { getDocument } from 'pdfjs-dist/legacy/build/pdf';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
